55e7a9ca925af75829edb2cedcf75e56d145b2cf
Jonny Boy <jrp12@hampshire.edu>
2014-02-13 22:15:50 -0500

    conf

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.width = 1100-- t.screen.width in 0.8.0 and earlier
+    t.window.height = 500 -- t.screen.height in 0.8.0 and earlier
+	t.modules.joystick = false
+    t.modules.physics = false
+end

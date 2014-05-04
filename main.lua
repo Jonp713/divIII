8d1d26a6c77e5023f6949e4e55f1e526a63b42b0
Jon Pedigo <jrp12@hampshire.edu>
2014-05-04 02:23:17 -0400

    advanced editor

--- main.lua
+++ main.lua
@@ -4,7 +4,12 @@ function love.load(args)
 	zpressable, qpressable, wpressable, apressabe, spressable, dpressable, ppressable, opressable = true
 	mpressable, npressable, bpressable, fpressable, gpressable, xpressable, cpressable, vpressable = true
 	
-	commapressable, periodpressable = true
+	equalpressable, commapressable, colonpressable, periodpressable, enterpressable, backspacepressable, leftpressable, rightpressable, uppressable, downpressable = true
+	
+	onepressable, twopressable = true
+	
+	
+	leftclick = true
 	
 	forward, backward = false
 	
@@ -18,6 +23,11 @@ function love.load(args)
 	toprint2 = 1
 	toprint3 = ''
 	toprint4 = ''
+	toprint5 = 1
+	toprint6 = 1
+	toprint7 = 1
+	
+	pause = false
 	
 	objects = {}
 
@@ -41,6 +51,8 @@ function love.load(args)
 	require("lua/AnAL")
 	--level editor
 	require('lua/editor')
+	--dialogue editor
+	require('lua/diaeditor')
 	--sections of the world
 	require('lua/section')
 	--sections of the world
@@ -55,6 +67,8 @@ function love.load(args)
 	require('lua/hater')
 	--dialogue events timed
 	require('lua/dialogue')
+	--objects
+	require('lua/object')
 	--npcs
 	require('lua/NPC')
 	--movement events timed
@@ -65,8 +79,6 @@ function love.load(args)
 	require('lua/door')
 	--cutscenes
 	require("lua/cinematic")
-	--objects
-	require('lua/object')
 	--load maps, create sections
 	require('lua/worldsetup')
 	--sets up,creates, lods characters
@@ -78,6 +90,9 @@ function love.load(args)
 	--player character
 	require('lua/hero')
 	
+	
+	--notes system
+	require('lua/notes')
 	--gui
 	require('lua/debug')
 	--debug
@@ -86,6 +101,13 @@ function love.load(args)
 	time()
 	
 	chars:set(2)
+	chars:diaChop()
+		
+	defaultfont = love.graphics.newFont('fonts/typewriter.ttf', 20 )
+	font2 = love.graphics.newFont(12) -- the number denotes the font size
+	thinfont = love.graphics.newFont('fonts/thin.ttf', 50 )
+
+	love.graphics.setFont(font2)
 		
 
 end
@@ -101,7 +123,7 @@ function love.update(dt)
 		--checks for keys to change objects and fastforwarding/rewinding in editor
 		editor:checkKeys(dt)
 		editor:selecting()
-	
+		diaeditor:checkKeys()
 	end
 	
 	--checks if keys ARENT pressed and fullscreen keychecks
@@ -109,6 +131,8 @@ function love.update(dt)
 	
 	--moves camera up and down...zooms camera
 	camera:checkKeys(dt)
+	
+	notes:checkKeys()
 	
 	--playerkeys and movements
 	player:update(dt)
@@ -178,6 +202,20 @@ function love.draw()
 	--display dialogues over everything else
 	dialogues()
 		
+		
+	--dragbox
+	if(editor.rwasdown and editorMode)then
+   
+   	 	love.graphics.rectangle("line", editor.boxx1, editor.boxy1, editor.boxxdrag - editor.boxx1, editor.boxydrag - editor.boxy1)
+
+	end
+	
+	if(editor.hiddenDrag and editorMode)then
+ 		
+		love.graphics.rectangle("line", editor.hiddenx1, editor.hiddeny1, editor.hiddenDragx - editor.hiddenx1, editor.hiddenDragy - editor.hiddeny1)
+		
+	end
+		
 	--unset the background
   	camera:unset()
   
@@ -188,7 +226,8 @@ function love.draw()
 	--opening.finished = true
 	
 	if(editorMode)then
-	
+		diaeditor:draw()
+		
 		editorDebug()
 	else
 		
